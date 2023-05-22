db68bc78670030e555fd251ea03a0c162f3e01fb
Reyes <Carlosreyes1803@gmail.com>
2023-05-22 08:29:51 -0600

    se genero la estructura  html para idicar al usuario si algo salio mal al guardar los datos

--- controladores/guardar.php
+++ controladores/guardar.php
@@ -2,7 +2,20 @@
 //Aca se usa el requiere para indicar que se va a agragar el archivo Cursos.php
 require '../../modelos/Cursos.php';
 ?>
-<!--  -->
+<!--Aca se genero la estructura para generar codigo html para que le indique al usuario si algo salio mal 
+al guardar los datos -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Guardado de datos</title>
+</head>
+<body>
 
+</body>
+</html>
 
 
