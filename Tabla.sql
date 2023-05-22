cc155b3dc04b738092e50686ad08aa2a88497b88
Reyes <Carlosreyes1803@gmail.com>
2023-05-22 07:58:08 -0600

    Creacion de tabla para la base de datos

---
+++ Tabla.sql
@@ -0,0 +1,8 @@
+--Creacion de tabla para la base de datos
+CREATE TABLE cursos(
+    nombre_curso CHAR(50) not null,
+    cantidad_periodos INT not null,
+    modulo_impartido INT not null,
+    nombre_instructor CHAR(50) not null,
+    creditos_curso DECIMAL not null
+);
