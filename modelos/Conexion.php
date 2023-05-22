74dc3fc7ed7e03b17fa9a9e005a2953bacc18288
Reyes <Carlosreyes1803@gmail.com>
2023-05-22 08:50:09 -0600

    Aca se realiza la conexion a la BD usando este enlace.

--- modelos/Conexion.php
+++ modelos/Conexion.php
@@ -1,1 +1,23 @@
 <!-- Establecer Conexion con la base de Datos -->
+<?php
+abstract class Conexion{
+    public static $conexion = null;
+
+    private static function conectar(){
+        try{
+            //Aca se realiza la conexion a la BD usando este enlace.
+            self::$conexion = new PDO('informix:host=host.docker.internal; service=9088; database=mdn; server=informix; protocol=onsoctcp;EnableScrollableCursors = 1','informix','in4mix'); 
+            // Definimos el manejo de las excepecones.
+            self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            // Si existira un error aparecera lo siguiente.
+            echo "Error de conexion de BD";
+            echo "<br>";
+            echo $e->getMessage();
+            exit;
+        }
+            //Colamos return para obtener un resultaddo.
+        return self::$conexion;
+    }
+
+    
