0b5ee0c5ea37581f6401acb8bc18670e7ae3acee
Reyes <Carlosreyes1803@gmail.com>
2023-05-22 08:56:27 -0600

    Crecion de las Clases para los objetos

--- modelos/Cursos.php
+++ modelos/Cursos.php
@@ -1,1 +1,14 @@
 <!-- Crecion de las Clases para los objetos -->
+<?php
+//Conectamos a Conexion.php
+require 'Conexion.php';
+//y cremoas la clase de curso.
+class Cursos extends Conexion{
+    public $nombre_curso;
+    public $cantidad_periodos;
+    public $modulo_impartido;
+    public $nombre_instructor;
+    public $creditos_curso;
+}
+
+    
