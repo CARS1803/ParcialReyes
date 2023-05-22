0a2f3b8e8959acf88cd14219ddfff8ce87d9d804
Reyes <Carlosreyes1803@gmail.com>
2023-05-22 08:59:03 -0600

    Inicializamos nuestros atributos del objeto con construct.

--- modelos/Cursos.php
+++ modelos/Cursos.php
@@ -9,6 +9,16 @@ class Cursos extends Conexion{
     public $modulo_impartido;
     public $nombre_instructor;
     public $creditos_curso;
-}
 
-    
+//Inicializamos nuestros atributos del objeto con construct.
+    public function __construct($argumento = [] )
+    {
+        $this->nombre_curso = $argumento['nombre_curso'] ?? null;
+        $this->cantidad_periodos = $argumento['cantidad_periodos'] ?? '';
+        $this->modulo_impartido = $argumento['modulo_impartido'] ?? '';
+        $this->nombre_instructor = $argumento['nombre_instructor'] ?? '';
+        $this->creditos_curso = $argumento['creditos_curso'] ?? '';
+
+    }
+
+}
